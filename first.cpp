f247088d06f8743f2e8bd112c9482b3fe08cbcbc
SushantAher02 <141741803+SushantAher02@users.noreply.github.com>
2024-03-01 11:16:10 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//    cout<<"hello coder Army\n";
+//    cout<<"hello coder Army1\n";
+
+//    cout<<3*2;
+
+// varible declare 4 byte
+    //  int a = 10 , b = 20;
+    //  cout<<a+b;
+
+
+    // char c = 'a';
+    // cout<<c;
+
+    // float f = 2.32;
+    // cout<<f;
+
+    // double d = 3.345;
+    // cout<<d;
+
+    // bool b = false;
+    // cout<<b;
+
+    int a = -32;
+    cout<<a;
+};
---
+++ second.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter your first number: ";
+    cin>>a;
+    cout<<"enter your second number: ";
+    cin>>b;
+    cout<<"your answer is: ";
+    
+};
+
