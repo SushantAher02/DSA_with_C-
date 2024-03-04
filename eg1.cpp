b1754310e125e80e5361cc2f7a01a50370b5c1f6
SushantAher02 <141741803+SushantAher02@users.noreply.github.com>
2024-03-04 08:31:08 +0530

    Add files via upload

--- eg1.cpp
+++ eg1.cpp
@@ -1,38 +1,34 @@
 #include<iostream>
 using namespace std;
 int main()
-{ 
-    // int row, col;
-    // int n;
-    // cout<<"Input number: ";
-    // cin>>n;
-    // for(row=1; row<=n; row=row+1)
+{
+    // int row,col;
+    // for(row=1; row<=5; row=row+1)
     // {
-    //     // space print
-    //    for(col=1; col<=n-row; col=col+1)
-    //    cout<<"  ";
-
-    //     // star print 
     //     for(col=1; col<=row; col=col+1)
     //     {
     //         cout<<"* ";
+
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int row,col;
+    // for(row=1; row<=5; row=row+1)
+    // {
+    //     for(col=1; col<=row; col=col+1)
+    //     {
+    //         cout<<col<<" ";
     //     }
     //     cout<<endl;
     // }
 
 
-    // int row, col;
-    // int n; 
-    // cout<<"Enter the Input number: ";
-    // cin>>n;
-    // for(row=1; row<=n; row=row+1)
-    //  {   
-    //     // space print
-    //     for(col=1; col<=n-row; col=col+1)
-    //     {
-    //         cout<<"  ";
-    //     }
-    //     // Number print 
+
+
+    // int row,col;
+    // for(row=1; row<=5; row=row+1)
+    // {
     //     for(col=1; col<=row; col=col+1)
     //     {
     //         cout<<row<<" ";
@@ -41,82 +37,66 @@ int main()
     // }
 
 
-        // int row, col;
-        // int n ;
-        // cout<<"Enter the input number: ";
-        // cin>>n;
-        // for(row=1; row<=n; row=row+1)
-        // {
-        //     // space print 
-        //     for(col=1; col<=n-row; col=col+1)
-        //     {
-        //         cout<<"  ";
+    // int row,col;
+    // for(row=1; row<=5; row=row+1)
+    // {
+    //     for(col=row; col>=1; col--)
+    //     {
+    //         cout<<col<<" ";
 
-        //     }
-        //     // number print
-        //     for(col=1; col<=row; col=col+1)
-        //     {
-        //         cout<<col<<" ";
-        //     }
-        //     cout<<endl;
-        // }
+    //     }
+    //     cout<<endl;
+    // }
 
 
-        // int row, col;
-        // int n;
-        // cout<<"Enter the input number: ";
-        // cin>>n;
-
-        // for(row=1; row<=n; row=row+1)
-        // {
-        //     // space print
-        //     for(col=1; col<=n-row; col=col+1)
-        //     {cout<<"  ";}
-        //     // char print
-        //     for(char name='A'; name<='A'+row-1; name=name+1)
-        //     {cout<<name<<" ";}
-        //     cout<<endl;
-        // }
+    // int row,col;
+    // char name;
+    // for(row=1; row<=5; row++)
+    // {
+    //     char name = 'a'+(row-1);
+    //     for(col=1; col<=row; col++)
+    //     {
+    //         cout<<name<<" ";
+    //     }
+    //     cout<<endl;
+  
+//   }
+      
 
 
-        int row, col;
+    //   int row,col;
+    //  for(row=1; row<=5; row++)
+    //  {
+    //     for(col=1; col<=5-(row-1); col++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    //  }
 
-        for(row=1; row<=5; row=row+1)
+
+
+
+    // int row,col;
+    // for(row=1; row<=5; row++)
+    // {
+    //     for(col=1; col<=5-(row-1); col++)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    int row,col;
+    for(row=1; row<=5; row++)
+    {
+        for(col=5; col>=5-(row-1); col--)
         {
-            //  space print
-            for(col=1; col<=5-row; col=col+1)
-            {cout<<"  ";}
-            // no print 
-            for(col=row; col<=1; col=col+1)
-            {cout<<row<<" ";}
-            cout<<endl;
+            cout<<col<<" ";
+
         }
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        cout<<endl;
+    }
 }
+
