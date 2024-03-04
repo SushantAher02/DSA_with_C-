e474a2ab562c7d32a292498b6a8b36f4ee431fb0
SushantAher02 <141741803+SushantAher02@users.noreply.github.com>
2024-03-04 08:29:58 +0530

    Add files via upload

---
+++ eg5.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int row,col;
+    // for(row=1; row<=5; row=row+1)
+    // {
+    //     char name='a'+ (row-1) ;
+    //     for(col=1; col<=5; col=col+1)
+    //     {
+    //         cout<<name<<" ";
+    //     }
+    //     cout<<endl;
+    //        }
+
+    // int row;
+    // for(row=1; row<=5; row=row+1)
+    // {
+    //     for(char col='a'; col<='e'; col=col+1)
+    //     {
+    //         cout<<col<<" ";
+
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    int row,col;
+    int count=1;
+    for(row=1; row<=5; row=row+1)
+    {
+      for(col=1; col<=5; col=col+1)
+      {
+        cout<<count<<" ";
+        count=count+1;
+      }
+      cout<<endl;
+    }
+}
---
+++ pattern_print.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    // int i,j;
+
+//     for(j=1; j<=5; j++){
+//         for(i=1; i<=5; i++){
+//             cout<<"*"<<" ";
+
+//         }
+//         cout<<endl;
+//     }
+
+int row,col;
+for(row=1;row<=5;row++)
+{
+    for(col=1;col<=5;col++)
+    {
+        cout<<"* ";
+
+    }
+    cout<<endl;
+}
+
+}
